21c1f69c981e80b6e5988ad5ced9a3a8d6683d2e
Vukan <platinumpies@gmail.com>
2015-11-27 18:23:11 -0500

    initial push

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('remixAdmin', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'toastr']);
+
+})();
