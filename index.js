8927d64217703ec1bb4761bff6389398b562a579
Vukan <platinumpies@gmail.com>
2015-11-27 19:10:19 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var proxy = require('./gulp/proxy');
+var app = express();
+
+app.set('port', (process.env.PORT || 9001));
+app.use(express.static(__dirname + '/dist'));
+app.use(proxy);
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running at localhost:' + app.get('port'));      
+});
