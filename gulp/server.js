db5d7385e4138eb16ecc10554f284bbaeae93d05
Vukan <platinumpies@gmail.com>
2015-11-27 19:42:45 -0500

    server

--- gulp/server.js
+++ gulp/server.js
@@ -33,7 +33,7 @@ function browserSyncInit(baseDir, browser) {
    *
    * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.0.5/README.md
    */
-  // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', proxyHost: 'jsonplaceholder.typicode.com'});
+   server.middleware = proxyMiddleware('/users', {target: 'http://remixadmin.herokuapp.com', proxyHost: 'remixadmin.herokuapp.com'});
 
   browserSync.instance = browserSync.init({
     startPath: '/',
